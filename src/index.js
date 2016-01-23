67d96f33c2209d90362b0fcd80a74a597b516aee
deepsweet <kir@soulshine.in>
2016-01-23 16:57:14 +0700

    :lipstick: hi, my name is Kir

--- src/index.js
+++ src/index.js
@@ -39,6 +39,7 @@ export default function({ types: t }) {
                     });
                 }
             },
+
             CallExpression(path) {
                 /*
                     <BEM block="test"></BEM>
