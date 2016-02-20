41dcf7da3761d0ad5058ab29787c07ccc43e352f
mistadikay <koltsov.d@gmail.com>
2016-02-20 16:27:32 +0700

    :space_invader: restore ability to run temporary tests

--- cases/test.js
+++ cases/test.js
@@ -7,7 +7,7 @@ import 'babel-register';
 
 const babel = require('babel-core'); // TODO ES6 import returns undefined
 const diff = require('diff');        // TODO ES6 import returns undefined
-const pluginPath = require.resolve('../src');
+const pluginPath = require.resolve('../lib');
 
 function normalizeLines(str) {
     return str.trimRight().replace(/\r\n/g, '\n');
@@ -53,7 +53,7 @@ function runTests() {
         .filter(testPath => fs.lstatSync(testPath).isDirectory())
         .forEach(testPath => {
             console.log(testPath);
-            runTest(testPath);
+            runTest(testPath, false);
         });
 
     console.log('external');
@@ -62,12 +62,12 @@ function runTests() {
         .filter(testPath => fs.lstatSync(testPath).isDirectory())
         .forEach(testPath => {
             console.log(testPath);
-            runTest(testPath, true);
+            runTest(testPath);
         });
 }
 
 if (process.argv.indexOf('--watch') >= 0) {
-    require('watch').watchTree(path.resolve(__dirname, '/../src'), function () {
+    require('watch').watchTree(path.resolve(__dirname, '/../lib'), function () {
         delete require.cache[pluginPath];
         clear();
         console.log('Press Ctrl+C to stop watching...');
